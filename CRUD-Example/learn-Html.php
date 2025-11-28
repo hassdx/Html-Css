cd51bbf9cf0c684a19f32295088b8625a128a60a
hassan elhouari <hassan.elhouari@stud.smk.lt>
2025-11-29 01:09:17 +0200

    Add files via upload
    An example of main page of my University website.

---
+++ CRUD-Example/learn-Html.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Stack+Sans+Text:wght@200..700&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="components/footer.php">
+    <title>SMK UNIVERSITY</title>
+</head>
+
+<header class="header">
+
+    <img class="logo" src="images/logo-black.svg">
+    <div class="requests">
+        <a href="#">Studies</a>
+        <a href="#">News</a>
+        <a href="#">Sor students</a>
+        <a href="#">Sineince</a>
+        <a href="#">For buisness</a>
+        <a href="#">|||</a>
+
+    </div>
+</header>
+
+
+<body>
+
+    <div class="container">
+        <img src="images/main1.jpg" alt="Image" class="image">
+
+        <div class="button-group">
+            <button>Log In</button>
+            <button>Register for Program</button>
+            <button>Register for Consultation</button>
+        </div>
+
+
+    </div>
+
+</body>
+
+
+<footer class="footer">
+    <div class="composents">
+        <img src="images/SMK_uni.jpg">
+        <div>
+            <h4>About SMK</h4>
+            <a href="#">About SMK</a>
+            <a href="#">Study Programmes</a>
+            <a href="#">Contacts</a>
+
+        </div>
+        <div>
+            <h4>Navigation</h4>
+            <a href="#">For students</a>
+            <a href="#">For businesses</a>
+            <a href="#">News</a>
+
+
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+
+</body>
+
+</html>
