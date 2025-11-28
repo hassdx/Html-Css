79aa607ae4565fffce68daff5ff9dd6f02227e19
hassan elhouari <hassan.elhouari@stud.smk.lt>
2025-11-29 01:06:50 +0200

    An example of moodle university platform.

---
+++ moudel_project/html.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Stack+Sans+Text:wght@200..700&display=swap"
+        rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Moudel</title>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <div class="container">
+        <img src="images/SMK_uni.jpg" alt="">
+        <div class=" info">
+            <input placeholder="Usernamee">
+            <input placeholder="Password">
+            <button>Log in</button>
+            <a href="#">Lost password?</a>
+            <hr style="width: 100%;", color="#e4e8ed">
+
+
+        </div>
+
+        <div class=" guest-part">
+
+
+            <span class="font">Some course may allow guest access</span>
+            <button class="button">Access as a guest</button>
+
+            <hr width="100%", color="#e4e8ed">
+        </div>
+
+
+        <div class="bottom">
+            <a class="lang" href="#">English (en) </a>
+            <hr class="rotate">
+
+            <style>
+                .rotate {
+                    transform: rotate(90deg);
+                    width: 20px;
+                    /* length of the line */
+                }
+            </style>
+            <button class="button">Cookies notice</button>
+
+
+        </div>
+
+
+    </div>
+
+</body>
+
+</html> 
+
+
+
+
+
+
+
+
